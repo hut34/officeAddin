eb808370bca7b9d3fbce33713d9c1dc4043249e3
pg <peter@hut34.io>
2020-03-16 15:04:59 +1100

    wip on re upload

--- hutExcel/src/taskpane/taskpane.js
+++ hutExcel/src/taskpane/taskpane.js
@@ -3,8 +3,6 @@
  * See LICENSE in the project root for license information.
  */
 
-/* global console, document, Excel, Office */
-
 Office.onReady(info => {
   if (info.host === Office.HostType.Excel) {
  // Determine if the user's version of Office supports all the Office.js APIs that are used in the tutorial.
@@ -16,39 +14,14 @@ Office.onReady(info => {
 
     document.getElementById("getDatasets").onclick = getDatasetsToDownload;
     document.getElementById("getDatasetButton").onclick = getDataset;
+    document.getElementById("uploadDataset").onclick = uploadDataset;
+
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
   }
 });
 
-
 var dataTable, datasetId, accessToken, idToken, apiURL
-
-function nameDataset() {
-
-    console.log('naming the puppy')
-    document.getElementById("datasetName").innerHTML="George";
-    document.getElementById("listOfDatasets").style = "display:block;";
-
-
-}
-
-function deleteDataset() {
-    console.log('deleting')
-
-    Excel.run(async function (context) {
-        dataTable.delete();
-        return context.sync();
-    })
-        .catch(function (error) {
-            console.log("Error: " + error);
-            if (error instanceof OfficeExtension.Error) {
-                console.log("Debug info: " + JSON.stringify(error.debugInfo));
-            }
-        });
-
-    return
-}
 
 async function getDatasetsToDownload() {
 
@@ -102,64 +75,6 @@ async function getDatasetsToDownload() {
 
     return
 
-}
-
-async function getDatasetsToPurchase() {
-
-    console.log('getting list of datasets to purchase')
-    //gets all available datasets from the hub
-    const response = await fetch('http://localhost:8080/user/getDatasetsToPurchase',
-        {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "accessToken": accessToken,
-                "token": idToken
-            })
-        });
-
-    const myJson = await response.json();
-    console.log(JSON.stringify(myJson));
-
-    return
-
-}
-
-async function getDatasets() {
-
-    console.log('getting datasets')
-    //gets all available datasets from the hub
-    const response = await fetch('http://localhost:8080/user/getDatasetsToDownload',
-        {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "accessToken": accessToken,
-                "token": idToken
-            })
-        });
-    const myJson = await response.json();
-    console.log(JSON.stringify(myJson));
-
-    /*
-    const response = await fetch(apiURL+'/alive', {
-        method: 'POST',
-        mode: 'no-cors',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-    });
-
-    const myJson = await response.json();
-    console.log(JSON.stringify(myJson));
-*/
 }
 
 async function getDataset() {
@@ -225,17 +140,45 @@ async function getDataset() {
     return
 }
 
+async function uploadDataset() {
+
+    console.log('uploading')
+    Excel.run(function (context) {
+
+        var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
+        var uploadTable = currentWorksheet.tables.getItem("Hut34Data");
+
+        // Get data from the header row
+        var headerRange = uploadTable.getHeaderRowRange().load("values");
+
+        // Get data from the table
+        var bodyRange = uploadTable.getDataBodyRange().load("values");
+
+        // Sync to populate proxy objects with data from Excel
+        return context.sync()
+            .then(function () {
+
+                var headerValues = headerRange.values;
+                var bodyValues = bodyRange.values;
+
+                console.log('we have the table, which is an update of '+datasetId)
+                console.log(headerValues)
+                console.log(bodyValues)
+
+            });
+    }).catch();
+
+
+}
+
 function createTable() {
 
 
   Excel.run(async function (context) {
-    // TODO1: Queue table creation logic here.
+
     var currentWorksheet = context.workbook.worksheets.getActiveWorksheet();
     expensesTable = currentWorksheet.tables.add("A1:D1", true /*hasHeaders*/);
     expensesTable.name = "ExpensesTable";
-
-    // TODO2: Queue commands to populate the table with data.
-
 
     expensesTable.getHeaderRowRange().values =
         [["Date", "Merchant", "Category", "Amount"]];
@@ -273,58 +216,4 @@ function createTable() {
           console.log("Debug info: " + JSON.stringify(error.debugInfo));
         }
       });
-}
-
-//tim's test function...
-
-
-function downloadDataset() {
-
-    let request = require("request");
-
-    Excel.run(function (context) {
-
-    //1. retrieve dataset from dataHut
-    var options = { method: 'POST',
-      url: 'https://hut34datahub.appspot.com/user/downloadFile',
-      headers:
-          { 'postman-token': '77d3bea5-f7dd-59be-7aee-9605aa7278ee',
-            'cache-control': 'no-cache',
-            'content-type': 'application/json' },
-      body:
-          { token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjRhOWEzMGI5ZThkYTMxNjY2YTY3NTRkZWZlZDQxNzQzZjJlN2FlZWEiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiVGltIE1jTmFtYXJhIiwicGljdHVyZSI6Imh0dHBzOi8vbGgzLmdvb2dsZXVzZXJjb250ZW50LmNvbS8tdVFSOXRjdTRUVTQvQUFBQUFBQUFBQUkvQUFBQUFBQUFBR28vVk03SUlJb2xKY1kvcGhvdG8uanBnIiwib3duZXIiOnRydWUsImFkbWluIjp0cnVlLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaHV0MzRkYXRhaHViIiwiYXVkIjoiaHV0MzRkYXRhaHViIiwiYXV0aF90aW1lIjoxNTczMzc0ODU4LCJ1c2VyX2lkIjoieWhYQmdTb3VINGJBTGdYd282VngwbW9LSGpOMiIsInN1YiI6InloWEJnU291SDRiQUxnWHdvNlZ4MG1vS0hqTjIiLCJpYXQiOjE1NzUwNzI3ODksImV4cCI6MTU3NTA3NjM4OSwiZW1haWwiOiJ0aW1AaHV0MzQuaW8iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJnb29nbGUuY29tIjpbIjExMzg3Nzk0Njc0Mjc1ODA3ODE0NyJdLCJlbWFpbCI6WyJ0aW1AaHV0MzQuaW8iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.RwfBBrZ2DKuk7rGCPF1EOCRbpW9kJ8NsLvnfp0OLMxZMpoFEVAJ3fXWifreGWIYHpcbH9b3iYszz0mrFOvxQIZxEsNZR6y78uYgKiZkwgxn8xhQbdVv19hVdZg89XCwUtre7Bkw7W_rAQpSDp1hEmarS9BrRYNtVtZTYaVmnnArJo5f3QCXBMNbbqGlIF5zFcxYMhvbAcfAJH2tm9TZ7zaJ7ewEZ09ejilnkXq3BwpozUznHxr85GcIEH0c4QmKIp5VqPCX_MjCEcYTCv6hdwruF2cYZvJW4WOPDTISJPULY1Qq-AVC6By52DRoHWcY_yTBPHvQ-9Uf1gBkqCqnlOQ',
-            accessToken: 'ya29.ImCwB_nN3D8bqInzWEH5J-aPaGgotTxt3Y8ZQSO1RS7cxES5J-OT5XWRlQdyVcuv-gkc4ZqrJcbo6v-2fm46jFroml5yzCJIimyY7aXcLQDtpF-qK6ke5-TTYLWii2tJwss',
-            datasetId: '3htIYCymXD8evnf84MfT' },
-      json: true };
-
-
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      console.log(body);
-    });
-
-    //todo calculate size of dataset rows and columns to set table size?
-    //todo find name of dataset to assign to hutDataset.name
-
-
-  })
-      .catch(function (error) {
-        console.log("Error Tom: " + error);
-        if (error instanceof OfficeExtension.Error) {
-          console.log("Debug info: " + JSON.stringify(error.debugInfo));
-        }
-      });
-}
-
-function webRequest(url, data) {
-    return new Promise(function (resolve, reject) {
-        fetch(url)
-            .then(function (response){
-                    return response.json();
-                }
-            )
-            .then(function (json) {
-                resolve(JSON.stringify(json.names));
-            })
-    })
 }
